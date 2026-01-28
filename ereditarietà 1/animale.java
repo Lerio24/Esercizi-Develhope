89d064f9a784f3114e4992f84440707b2facded9
Andrea Valerio Caricchia <126721538+Lerio24@users.noreply.github.com>
2026-01-28 15:08:07 +0100

    Delete ereditarietà 1 directory

--- ereditarietà 1/animale.java
+++
@@ -1,5 +0,0 @@
-public class Animale {
-    public void faiIlVerso() {
-        System.out.println("L'animale fa un verso generico.");
-    }
-}
--- ereditarietà 1/gatto.java
+++
@@ -1,1 +0,0 @@
-public class Gatto extends Animale { @Override public void faiIlVerso() { System.out.println("Miao!"); } }
