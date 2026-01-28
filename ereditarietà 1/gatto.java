f91dc3dca773ec43f144eba1f115c62f7addacaa
Andrea Valerio Caricchia <126721538+Lerio24@users.noreply.github.com>
2026-01-28 15:05:22 +0100

    Add files via upload

---
+++ ereditarietà 1/gatto.java
@@ -0,0 +1,1 @@
+public class Gatto extends Animale { @Override public void faiIlVerso() { System.out.println("Miao!"); } }
---
+++ ereditarietà 1/main.java
@@ -0,0 +1,23 @@
+public class Animale {
+    public void faiIlVerso() {
+        System.out.println("L'animale fa un verso generico.");
+    }
+}
+
+class Gatto extends Animale {
+    @Override
+    public void faiIlVerso() {
+        System.out.println("Miao!");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+
+        Animale a = new Animale();
+        Gatto g = new Gatto();
+
+        a.faiIlVerso();
+        g.faiIlVerso();
+    }
+    }
