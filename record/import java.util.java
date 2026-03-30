449f5609fbe05d7372670516fa9d7738790b4640
Andrea Valerio Caricchia <126721538+Lerio24@users.noreply.github.com>
2026-03-30 16:40:34 +0200

    Add files via upload

---
+++ record/import java.util.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Point p = new Point(10, 20);
+
+        System.out.println("Coordinata X: " + p.x());
+        System.out.println("Coordinata Y: " + p.y());
+        System.out.println("Punto completo: " + p);
+    }
+}
